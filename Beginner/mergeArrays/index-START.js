56acf1d28baa962cbe90fd0a75188877ac4dc910
Arpad Bencze <bencze.arpad.21@gmail.com>
2020-03-30 23:39:07 +0200

    performance comparison for array merging algorithms

--- Beginner/mergeArrays/index-START.js
+++ Beginner/mergeArrays/index-START.js
@@ -4,8 +4,8 @@
  E.g mergeArrays([1,2,3,3,3], [1,4,5,2]) // should return [1,2,3,4,5]
 */
 
-function mergeArrays(...arrays) {
-
+function mergeArraysWithSet(...arrays) {
+    //2nd fastest
     let jointArray = []
     
     arrays.forEach(array => {
@@ -13,9 +13,34 @@ function mergeArrays(...arrays) {
     });
 
     return [...new Set([...jointArray])]
-
-    
 }
 
+function mergeArraysWithFilter(...arrays) {
+    //Fastet solution
+    let jointArray = []
+
+    arrays.forEach(array => {
+        jointArray = [...jointArray, ...array]
+    })
+    const uniqueArray = jointArray.filter((item,index) => jointArray.indexOf(item) === index)
+    return uniqueArray
+}
+
+function mergeArraysWithReduce(...arrays) {
+    //3rd fastest
+    let jointArray = []
+
+    arrays.forEach(array => {
+        jointArray = [...jointArray, ...array]
+    })
+    const uniqueArray = jointArray.reduce((newArray, item) =>{
+        if (newArray.includes(item)){
+            return newArray
+        } else {
+            return [...newArray, item]
+        }
+    }, [])
+    return uniqueArray
+}
 
 module.exports = mergeArrays
