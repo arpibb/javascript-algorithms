6656c51de636b23724b6b1e7ee23d407885f9973
Arpad Bencze <bencze.arpad.21@gmail.com>
2020-03-30 23:08:40 +0200

    performance comparison for array chunking algorithms

--- Beginner/chunkArray/index-START.js
+++ Beginner/chunkArray/index-START.js
@@ -5,8 +5,36 @@
 */
 
 function chunkArray(array, size) {
-    // Code goes here
+    //Fastest solution
+    let resultArr = []
+    for(let i = 0; i < array.length; i += size){
+        resultArr.push(array.slice(i,i+size))
+    }
+
+    return resultArr
+}
+
+function chunkArraySplice(array, size) {
+    //2nd slowest
+    let result = []
+    let arrayCopy = [...array]
+    while (arrayCopy.length > 0) {
+        result.push(arrayCopy.splice(0, size))
+    }
+    return result
+}
+
+function chunkArrayRecursion(array,size){
+    //3rd slowest
+    if(array.length <=size){
+        return [array]
+    }
+    else {
+        return [array.slice(0,size),...chunkArray(array.slice(size),size)]
+    }
 }
 
 
 module.exports = chunkArray
+
+chunkArray([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13], 5)
