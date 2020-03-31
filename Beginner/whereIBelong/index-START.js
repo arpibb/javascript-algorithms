c9c1a4052e28b72a1f7053df143b0e17c13662dc
Arpad Bencze <bencze.arpad.21@gmail.com>
2020-03-31 14:11:36 +0200

    where a number belongs in an array alogrithms performance comparison

--- Beginner/whereIBelong/index-START.js
+++ Beginner/whereIBelong/index-START.js
@@ -9,9 +9,43 @@ than 1(index 0), but less than 2(index 1).
 
 
 
-function whereIBelong(arr, num) {
-   // Code goes here
+function whereIBelongForLoop1(arr, num) {
+   //cca 95% slower then the first
+   arr.sort((a,b)=> a-b)
 
+   for(let i = 0; i < arr.length; i++){
+      if(arr[i] >= num){
+         return i
+      }
+   }
+   return arr.length
+}
+function whereIBelongForLoop2(arr, num) {
+   //Fastest solution out of the 4
+   var counter = 0
+   for (i = 0; i < arr.length; i++) {
+       if (num > arr[i]) {
+           counter++;
+       }
+   } 
+   return counter
+}
+
+function whereIBelongWhile(arr, num) {
+   //cca 95% slower then the first
+   arr.sort((a,b)=> a-b)
+   let counter = 0
+   while(num > arr[counter]){
+      counter++
+   }
+   return counter
+}
+
+function whereIBelong(arr,num){
+   //cca 96% slower then the first, slowest
+   arr.push(num)
+   arr.sort((a,b)=> a-b)
+   return arr.indexOf(num)
 }
 
 
